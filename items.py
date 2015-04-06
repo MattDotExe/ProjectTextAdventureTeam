faac1b298d5ce5e4eb3bfb0fbd15c338f0aa8570
MattDotExe <Mbrittain7134@gmail.com>
2015-04-06 13:24:45 -0500

    Update items.py

--- items.py
+++ items.py
@@ -37,14 +37,14 @@ class Sword(Weapon):
                          value=10,
                          damage=10)
                          
-class Brimstone(Weapon):
+class Needed(Weapon):
     def __init__(self):
-        super().__init__(name="Brimstone",
-                         description="A stone completely identical to the average rock, although strangely enough you feel as though it has a skin-like texture to it. It seems to often pus out blood in a straight line at random.",
+        super().__init__(name="Needed",
+                         description="",
                          value=0,
-                         damage=7)
+                         damage=0)
 
-class Brimstone(Weapon):
+class Mace(Weapon):
     def __init__(self):
         super().__init__(name="Mace",
                          description="Back in the War of the Village, this very rusty Mace was used to fight off the Skeletons from The Ruins. Most people think that weapons like this should be with the soilders at the memorial, but the Weaponsmith insists that if something like that were to happen again, people would want them to be here.",
